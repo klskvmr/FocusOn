31cbf33ff244707e296ba8f833c76565b31f6618
mariia.koloskova <mariia.koloskova@eltex.loc>
2021-01-16 22:49:23 +0700

    init commit

---
+++ focus-on-backend/src/main/kotlin/com/focuson/focusonbackend/FocusOnBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.focuson.focusonbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FocusOnBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<FocusOnBackendApplication>(*args)
+}
---
+++ focus-on-backend/src/test/kotlin/com/focuson/focusonbackend/FocusOnBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.focuson.focusonbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FocusOnBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
